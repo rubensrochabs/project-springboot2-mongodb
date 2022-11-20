9d7dfa66e09febcad8cf9a8f8d552d4855a79dc3
rubensrochabs <rubensrochabs@gmail.com>
2022-11-20 15:09:19 -0300

    User findById, exception handling

--- src/main/java/com/rubensrocha/projectmongo/services/UserService.java
+++ src/main/java/com/rubensrocha/projectmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.rubensrocha.projectmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rubensrocha.projectmongo.domain.User;
 import com.rubensrocha.projectmongo.repositories.UserRepository;
+import com.rubensrocha.projectmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,9 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
