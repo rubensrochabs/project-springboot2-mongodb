1f69f75ab196cb4af3c7fc330745d42686780b36
rubensrochabs <rubensrochabs@gmail.com>
2022-11-20 17:29:08 -0300

    Endpoint to return a user's posts

--- src/main/java/com/rubensrocha/projectmongo/resources/UserResource.java
+++ src/main/java/com/rubensrocha/projectmongo/resources/UserResource.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.rubensrocha.projectmongo.domain.Post;
 import com.rubensrocha.projectmongo.domain.User;
 import com.rubensrocha.projectmongo.dto.UserDTO;
 import com.rubensrocha.projectmongo.services.UserService;
@@ -61,6 +62,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
