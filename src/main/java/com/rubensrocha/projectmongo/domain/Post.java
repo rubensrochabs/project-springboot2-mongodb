273e192a47fbbe221ec36a582d5be88d5a8560f1
rubensrochabs <rubensrochabs@gmail.com>
2022-11-27 15:13:15 -0300

    Simple query with query methods

--- src/main/java/com/rubensrocha/projectmongo/domain/Post.java
+++ src/main/java/com/rubensrocha/projectmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.rubensrocha.projectmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.rubensrocha.projectmongo.dto.AuthorDTO;
+import com.rubensrocha.projectmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -19,6 +22,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -71,7 +76,11 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
-
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -88,5 +97,5 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
